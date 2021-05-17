8089604f134002c98130c2552b04472d825a30db
Mmalleret <milimalleret1@gmail.com>
2021-05-17 13:20:20 -0300

    Dejamos el proyecto listo para arrancar a trabajar

---
+++ proyectorn/src/App.js
@@ -0,0 +1,27 @@
+import React, { Component } from 'react';
+
+import { 
+  View,
+  Text
+} from "react-native";
+
+export default class App extends Component {
+  
+constructor() {
+  super();
+  this.state = {
+
+  }
+}  
+
+
+  render (){
+    return (
+    <View>
+      <Text>funciona todo ok!</Text>
+    </View>
+  
+  
+  )}  
+
+}
