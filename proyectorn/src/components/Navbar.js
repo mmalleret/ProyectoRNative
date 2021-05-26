262779606cec20531f7ac524f5f4e28cc7a2d789
zoeHamraa <65314353+zoeHamraa@users.noreply.github.com>
2021-05-25 21:48:46 -0300

    Ahora si Nemo tiene su primer navbar hashtagblessed

--- proyectorn/src/components/Navbar.js
+++ proyectorn/src/components/Navbar.js
@@ -17,16 +17,11 @@ constructor() {
 
   render (){
     return (
-<<<<<<< HEAD
     <View style={{backgroundColor: "lightblue",
     justifyContent: "center",
     alignItems: "center", marginTop: 50, fontSize:40, padding:30}}>
         <Menu></Menu>
         <Text style={styleText}>Nemo</Text>
-=======
-    <View>
-        <Text>HOLA COMO ESTAS</Text>
->>>>>>> 8bed1912bd8cc4632732e8771428909969751405
     </View>
   
   
