f4f3e6cc63c0c91f2e2dd268144482c1e302f10b
Clara Kearney <ckearney@udesa.edu.ar>
2021-06-08 17:12:16 -0300

    Componetizando delete

--- proyectorn/src/screens/Screen_Imports.js
+++ proyectorn/src/screens/Screen_Imports.js
@@ -59,20 +59,6 @@ async storeData(value){
   }
 }
 
-async deleteData(id) {
-  try {
-    let deleteContacto = this.state.almacenar.filter((dato) => {return dato.login.uuid !== id})
-    this.setState({almacenar : deleteContacto})
-    
-  } catch(e) {
-    console.log(e)
-  }
-}
-//Sobreeescribir contactos almacenando todos los contactos menos el que elimine -> setItem(contactos)
-// saveItem pero la key no es contacto sino borrados (cambiar save por set)
-
-
-
   render (){
     return (
     <View>
